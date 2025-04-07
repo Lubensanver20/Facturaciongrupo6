10889598b74b14566398e31c3606ac013325fe7a
unknown <luisbenjaminsanchezvergara@gmail-com>
2025-04-07 17:15:48 -0500

    primer commit

---
+++ src/main/java/com/mycompany/facturaciongrupo6/Facturaciongrupo6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.facturaciongrupo6;
+
+/**
+ *
+ * @author ALUMNO
+ */
+public class Facturaciongrupo6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
